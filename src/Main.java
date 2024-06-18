c5569d9ff2e4d36398f43bc8f1429b7e6065c463
abd1 <abd1@gmail.com>
2024-06-18 15:43:57 +0300

    add print array of int numbers

--- src/Main.java
+++ src/Main.java
@@ -9,4 +9,9 @@ public class Main {
             System.out.println("number: "+arr[i]);
         }
     }
+    public static void printDouble(double [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("number: "+arr[i]);
+        }
+    }
 }
