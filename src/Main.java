5a23453fa54678f2aac352c6e0562c71e3538a56
abd1 <abd1@gmail.com>
2024-06-18 15:48:34 +0300

    add function print array of Strings

--- src/Main.java
+++ src/Main.java
@@ -4,4 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("develop v2");
     }
+    public static void printStrings(String [] arr){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("number: "+arr[i]);
+        }
+    }
 }
